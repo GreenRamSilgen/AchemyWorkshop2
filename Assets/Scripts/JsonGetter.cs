0da0d73f87f81ce1f48c0cdb2d80c7dfd7b738bb
lukeren314 <lukeren314@gmail.com>
2019-10-28 19:48:51 -0700

    getting ready to add recipes

---
+++ Assets/Scripts/JsonGetter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonGetter : MonoBehaviour
+{
+    List<Item> getItems()
+    {
+        return (JsonUtility.FromJson<ItemList>((Resources.Load("SimpleResources") as TextAsset).text)).Items;
+    }
+}
