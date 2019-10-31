0278c5ac2d01b5840307ec2a301b2a6c679d0f1b
lukeren314 <lukeren314@gmail.com>
2019-10-30 17:12:46 -0700

    added retractable recipe menu

---
+++ Assets/RecipesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipesController : MonoBehaviour
+{
+    public Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetRecipesMenuTrigger()
+    {
+        animator.SetTrigger("RecipesMenu");
+    }
+}
