d51e8a1d5511a81694ba55fcb35316860823dde3
lukeren314 <lukeren314@gmail.com>
2019-11-04 13:15:45 -0800

    adding in items

--- Assets/Scripts/CraftingController.cs
+++ Assets/Scripts/CraftingController.cs
@@ -5,10 +5,48 @@ using System.IO;
 public class CraftingController : MonoBehaviour
 {
     private string resourcesFileName = "Data/Resources.json";
-    private string recipesFileName = "Data/Recipes.json";
-    // Start is called before the first frame update
+    public ItemList ItemList = new ItemList();
+    public List<GameObject> ItemObjects = new List<GameObject>();
+    public GameObject template;
+    public Transform ResourcePanelTransform;
+/*    private string recipesFileName = "Data/Recipes.json";
+*/    // Start is called before the first frame update
     void Start()
     {
+        TextAsset asset = Resources.Load("SimpleResources") as TextAsset;
+        if (asset != null)
+        {
+            //Debug.Log(jsonString);
+            ItemList = JsonUtility.FromJson<ItemList>(asset.text);
+            for(int i = 0; i < ItemList.Items.Count; i++)
+            {
+                ItemObjects.Add(Instantiate(template, ResourcePanelTransform));
+            }
+            RectTransform CanvasRectTransform = gameObject.transform as RectTransform;
+            Debug.Log("hi " + CanvasRectTransform.rect.height);
+            Debug.Log("hello " + gameObject.transform.localScale.y);
+            for(int i = 0; i < ItemObjects.Count; i++)
+            {
+                ItemObjects[i].transform.position = new Vector2(32+i*46%184, CanvasRectTransform.rect.height/2 - (i/4 * 32));
+            }
+            /*foreach (Item item in ItemList.Items)
+            {
+                Debug.Log(item.itemID);
+                Debug.Log(item.itemLoc);
+                Debug.Log(item.itemName);
+                Debug.Log(item.itemTier);
+                Debug.Log(item.A1Name);
+                Debug.Log(item.A1Amt);
+                Debug.Log(item.A2Name);
+                Debug.Log(item.A2Amt);
+                Debug.Log(item.A3Name);
+                Debug.Log(item.A3Amt);
+            }*/
+        }
+        else
+        {
+            print("Asset is null");
+        }
     }
 
     // Update is called once per frame
@@ -17,7 +55,7 @@ public class CraftingController : MonoBehaviour
         
     }
 
-    void LoadData()
+    /*void LoadData()
     {
         string filePath1 = Path.Combine(Application.streamingAssetsPath, resourcesFileName);
         string filePath2 = Path.Combine(Application.streamingAssetsPath, recipesFileName);
@@ -34,5 +72,5 @@ public class CraftingController : MonoBehaviour
             Debug.LogError("Can't load game data!");
         }
 
-    }
+    }*/
 }
