54f5ca1e55d1dcfeaea5f93adc0d139b1f0c8de2
GreenRamSilgen <ps3iswow@gmail.com>
2019-11-12 21:12:03 -0800

    Escape Menu Added
    -Basic Functional Escape Menu

---
+++ Assets/Scripts/EscScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscScript : MonoBehaviour
+{
+    public static bool Pause = false;
+    public GameObject escMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Pause)
+            {
+                CloseMenu();
+            }
+            else
+            {
+                OpenMenu();
+            }
+        }
+    }
+    
+    public void CloseMenu()
+    {
+        Pause = false;
+        escMenu.SetActive(false);
+    }
+
+    public void OpenMenu()
+    {
+        Pause = true;
+        escMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Bid you farewell!");
+        Application.Quit();
+    }
+}
