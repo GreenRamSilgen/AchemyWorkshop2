4d885bfca847ea06fdbcd911253a1b2b22c9399c
Fuyu <qunihan@gmail.com>
2019-11-16 22:06:51 -0800

    updates eod display

---
+++ Assets/Scripts/EndOfDayDisplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndOfDayDisplay : MonoBehaviour
+{
+    public Text ledger;
+    public Text day;
+
+    // Use this for initialization
+    void Start()
+    {
+        ledger.text = "    ledger:";
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    int calcHireCost()
+    {
+        //foreach (object kv in Global.gatherers)
+        //{
+
+        //}
+        
+        return 0;
+    }
+}
