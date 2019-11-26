c4a49896a5df060e0d7fc04c18f38fe39cd4bcc4
Fuyu <qunihan@gmail.com>
2019-11-25 20:13:18 -0800

    fxing merge issue

--- Assets/Scripts/EndOfDayDisplay.cs
+++ Assets/Scripts/EndOfDayDisplay.cs
@@ -6,27 +6,15 @@ using System.Linq;
 
 public class EndOfDayDisplay : MonoBehaviour
 {
-<<<<<<< HEAD
-    public Text ledger;
 
-=======
-    public Ledger ledger;
-    public Text day;
->>>>>>> 2d80daf3b13e63024b0f132de7274cf5cd0b56b4
+    public Text ledger;
 
     // Use this for initialization
     void Start()
     {
-<<<<<<< HEAD
     
         ledger.text = "       LEDGER:\n\nHiring Cost:  - " + calcHireCost() + "\n\n" + "Rent:            - 50\n\n" + "Potions:       + "
                         +"---------------------\nTotal:          ";
-=======
-        day.text = "End of Day " + Global.day;
-        
-                        //+"---------------------\nTotal:          ";
->>>>>>> 2d80daf3b13e63024b0f132de7274cf5cd0b56b4
-        
 
     }
 
@@ -36,7 +24,7 @@ public class EndOfDayDisplay : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P))
         {
             //ledger.cost.text = "YO";
-            ledger.cost.text = "Hiring Cost:  - " + calcHireCost() + "\n\n" + "Rent:            - 50\n\n" + "Potions:       + ";
+            ledger.text = "Hiring Cost:  - " + calcHireCost() + "\n\n" + "Rent:            - 50\n\n" + "Potions:       + ";
         }
     }
 
