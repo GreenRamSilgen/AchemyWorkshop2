4a9c1c369d57463aa4c7960bed3f1f0bd5ea4bdd
Fuyu <qunihan@gmail.com>
2019-11-25 20:47:22 -0800

    merge issue

--- Assets/Scripts/EndOfDayDisplay.cs
+++ Assets/Scripts/EndOfDayDisplay.cs
@@ -24,7 +24,7 @@ public class EndOfDayDisplay : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P))
         {
             //ledger.cost.text = "YO";
-            ledger.cost.text = "Hiring Cost:  - " + calcHireCost() + "\n\n" + "Rent:            - 50\n\n" + "Potions:       + ";
+            ledger.text = "Hiring Cost:  - " + calcHireCost() + "\n\n" + "Rent:            - 50\n\n" + "Potions:       + ";
         }
     }
 
